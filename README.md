f8b42a68ce115e1a0f7dcca2b18b98e8e2723aa8
Tomas <t.uzdavinys@gmail.com>
2021-11-28 13:17:06 +0200

    price devaluation displayed

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-# Autoplius car depretiation dash boad
-
-**Placeholder text below**
+# `autoplius.lt` car price devaluation dashboard
 
 Website demo: https://autoplius-deval.herokuapp.com/
 
-This repository is made out of 2 pats:
+## Description
+
+This repository is made out of 2 parts:
 
 * dash app,
-* iJupyter notebooks
+* iJupyter notebooks.
 
-Project structure:
+## Project stucture
 
+Dash app:
+
+* `app.py` main python file.
+* `Procfile` filne needed to host website on `heroku.com`.
+
+Notebooks `/Notebooks`:
+
+* `data_scraping_explained.ipynb` describes how data was scraped from autoplius.lt.
+* `/images` sub-folder for storing images for notebooks locally.
+
+## Project requirements
+
+`requirements.txt` file contains list of required libraries to run/host app on local machine.
--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from dash import Dash, dcc, html, no_update
 from dash.dependencies import Input, Output
 import pandas as pd
 
-# read datata from dropbox link
-df = pd.read_csv('https://www.dropbox.com/s/yxrjr9fai5zub3c/2011_cars_test.csv?dl=1', index_col=0)
-
+# read .csv from dropbox link.
+df_png = pd.read_csv('https://www.dropbox.com/s/b4nxy7zozz7fqtq/png_database.csv?dl=1')
 # generate car id dictionary for dropdown menu
-car_name_dict = [{'label': _, 'value': _} for _ in df.Car.unique()]
+car_name_dict = [{'label': _, 'value': _} for _ in df_png.Car.unique()]
+# reset index for simpler data accessing
+df_png.set_index(['Car', 'Year_made'], inplace=True)
 
 
 app = Dash(__name__,
@@ -16,7 +17,7 @@ server = app.server
 
 
 app.layout = html.Div([
-    html.H4('Car selection- placeholder'),
+    html.H4('Car selection'),
     dcc.Dropdown(
         id='car-name-drop-menu',
         options=car_name_dict,
@@ -24,7 +25,7 @@ app.layout = html.Div([
         clearable=False,
         placeholder="Select car"
     ),
-    html.H4('Year made- placeholder'),
+    html.H4('Year'),
     dcc.Dropdown(
         id='car-year-drop-menu',
         options=[],
@@ -32,6 +33,8 @@ app.layout = html.Div([
         clearable=False,
         placeholder="Select the year car was made"
     ),
+    html.Hr(),
+    html.Img(id='deval-auto-plius-img', src=''),
 ])
 
 
@@ -42,21 +45,33 @@ app.layout = html.Div([
 def update_year_made(car_name):
     """
     Updates drop down list with years car was made.
-    Include only those years for which data/.png files are available
+    Dropdown menu value is set to oldest car made.
     Input:
         car_name, str
     Return:
         dict, years that car was made
     """
-    global df
+    global df_png
     # don't update during initial launch
     if car_name == 'car-name':
         return no_update
     # get only unique values for specific car years
-    years = df.loc[df.Car == car_name, 'Year_made'].unique()
+    years = df_png.loc[car_name].index
     years = [{'label': year, 'value': year} for year in years]
-    # update with earliest available years
+    # update with oldest available years
     return years, years[0]['value']
+
+
+@app.callback(
+    [Output(component_id='deval-auto-plius-img', component_property='src')],
+    [Input(component_id='car-name-drop-menu', component_property='value'),
+     Input(component_id='car-year-drop-menu', component_property='value')])
+def autoplius_png(car_name, year_made):
+    global df_png
+    if year_made == 'year' or car_name == 'car-name':
+        return no_update
+    else:
+        return [df_png.loc[(car_name, int(year_made))].png_url]
 
 
 if __name__ == '__main__':
